d5a5fc695e58ee7e7c39be408ecfb90fdd10e3f4
Douglas360 <douglas_henriqueduarte@hotmail.com>
2024-02-12 14:32:52 -0300

    fix bugs

--- src/App.js
+++ src/App.js
@@ -12,14 +12,64 @@ import TokenMaster from "./abis/contracts/TokenMaster.sol/TokenMaster.json";
 
 // Config
 import config from "./config.json";
-//const networkData = TokenMaster.networks[networkId];
 
 function App() {
+  const [provider, setProvider] = useState(null);
+  const [account, setAccount] = useState("");
+
+  const [tokenMaster, setTokenMaster] = useState(null);
+
+  const loadWeb3 = async function loadWeb3() {
+    // Initialize Web3Provider
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    // Set provider
+    setProvider(provider);
+
+    // Fetch the contract
+    const contract = new ethers.Contract(
+      "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+      TokenMaster.abi,
+      provider // Pass provider here
+    );
+
+    contract.connect(provider.getSigner());
+
+    const tokenMaster = new ethers.Contract(
+      contract,
+      TokenMaster.abi,
+      provider
+    );
+    setTokenMaster(tokenMaster);
+
+    //const contractWithSigner = contract.connect(provider.getSigner());
+    const totalOccasions = await tokenMaster.totalOccasions();
+
+    console.log({ totalOccasions });
+
+    // Fetch the account
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    const account = ethers.utils.getAddress(accounts[0]);
+    setAccount(account);
+
+    // Refresh the account
+    window.ethereum.on("accountsChanged", (accounts) => {
+      const account = ethers.utils.getAddress(accounts[0]);
+      setAccount(account);
+    });
+  };
+
+  useEffect(() => {
+    loadWeb3();
+  }, []);
+
   return (
     <div>
       <header>
+        <Navigation account={account} setAccount={setAccount} />
         <h2 className="header__title">
-          <strong>Welcome to Tokenmaster</strong>
+          <strong>Event </strong>ticket
         </h2>
       </header>
     </div>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,50 @@
-import { ethers } from 'ethers'
+import { ethers } from "ethers";
 
 const Navigation = ({ account, setAccount }) => {
+  const connectWallet = async () => {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    const account = ethers.utils.getAddress(accounts[0]);
+    setAccount(account);
+  };
 
   return (
     <nav>
+      <div className="nav__brand">
+        <h1>tokenmaster</h1>
+        <input
+          className="nav__search"
+          type="text"
+          placeholder="Find millions of"
+        />
 
+        <ul className="nav__links">
+          <li>
+            <a href="/">Concerts</a>
+          </li>
+          <li>
+            <a href="/">Sports</a>
+          </li>
+          <li>
+            <a href="/">Theater</a>
+          </li>
+          <li>
+            <a href="/">More</a>
+          </li>
+        </ul>
+      </div>
+      {account ? (
+        <button type="button" className="nav__connect">
+          {account.slice(0, 6)}...{account.slice(account.length - 4)}
+        </button>
+      ) : (
+        <button type="button" className="nav__connect" onClick={connectWallet}>
+          Connect Wallet
+        </button>
+      )}
     </nav>
   );
-}
+};
 
 export default Navigation;
